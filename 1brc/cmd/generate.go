9eef8a79499e867119b9c8e1dfa7a723714b2d63
Richard Marchesi <richard.marchesi@gmail.com>
2024-07-22 02:08:29 +0100

    added skeleton of generate cmd

--- 1brc/cmd/generate.go
+++ 1brc/cmd/generate.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -10,11 +9,72 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// const enum of city codes.
+const (
+	LAX = iota
+	DXB
+	JFK
+	NRT
+	SYD
+	CDG
+	BOM
+	YYZ
+	HKG
+	GRU
+	FRA
+	MEX
+	AMS
+	CPT
+	MUC
+	ICN
+	SFO
+	ZRH
+	DEL
+	JNB
+)
+
+// Define a type for CityCode.
+type CityCode [3]byte
+
+// Map of CityCode.
+var cities = map[int]CityCode{
+	LAX: {'L', 'A', 'X'},
+	DXB: {'D', 'X', 'B'},
+	JFK: {'J', 'F', 'K'},
+	NRT: {'N', 'R', 'T'},
+	SYD: {'S', 'Y', 'D'},
+	CDG: {'C', 'D', 'G'},
+	BOM: {'B', 'O', 'M'},
+	YYZ: {'Y', 'Y', 'Z'},
+	HKG: {'h', 'K', 'G'},
+	GRU: {'G', 'R', 'U'},
+	FRA: {'F', 'R', 'A'},
+	MEX: {'M', 'E', 'X'},
+	AMS: {'A', 'M', 'S'},
+	CPT: {'C', 'P', 'T'},
+	MUC: {'M', 'U', 'C'},
+	ICN: {'I', 'C', 'N'},
+	ZRH: {'Z', 'R', 'H'},
+	DEL: {'D', 'E', 'L'},
+	JNB: {'J', 'N', 'B'},
+}
+
+// Encapsulate weather a station reading.
+type measurement struct {
+	CityCode
+	temperature float32
+}
+
+// 1) loop for upto 1 billion;
+// 2) generate random float between -50.0-50.0;
+// 3) get random citycode;
+// 4) write out dat to file;
+
 // generateCmd represents the generate command
 var generateCmd = &cobra.Command{
 	Use:   "generate",
 	Short: "Generate new data file that can be used by the run command",
-        Run: func(cmd *cobra.Command, args []string) {
+	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("generate:TODO")
 	},
 }
