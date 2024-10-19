42dc2b58938d5f03ec5fefc4407a50f29d06d862
Richard Marchesi <richard.marchesi@gmail.com>
2024-10-20 00:46:42 +0100

    esc updates

--- infra/my-aws-nest/__main__.py
+++ infra/my-aws-nest/__main__.py
@@ -1,10 +1,19 @@
-"""An AWS Python Pulumi program"""
+# Pumumi Python program to manage my-aws-nest, provides baseline configurable infra.
 
 import pulumi
 from pulumi_aws import s3
+import random
+
+# Create a configuration object to access the stack's config.
+config = pulumi.Config()
+
+# Get s3_base config.
+s3_base = config.require("s3-base") 
+
+# mimic UIIX RAND.
+unix_rand = random.randint(0, 32767)
 
 # Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
-
+bucket = s3.Bucket(f"{s3_base}{unix_rand}")
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
